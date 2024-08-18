9fda8b19e47d26e0493e6d109254b601c3ce6111
cornzzy <116248852+cornzzy@users.noreply.github.com>
2024-08-18 10:26:03 +0330

    Update main.go

--- main.go
+++ main.go
@@ -1,221 +1,221 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"log"
-	"net"
-	"os"
-	"os/exec"
-	"strings"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "os"
+    "os/exec"
+    "strings"
+    "net"
+    "crypto/rand"
+    "encoding/base64"
+    "github.com/google/uuid"
 )
 
 const (
-	wgConfPath   = "/etc/wireguard/wg0.conf"
-	paramsPath   = "/etc/wireguard/params"
-	wgBinary     = "/usr/bin/wg"
-	ipv4Subnet   = "10.25.0.0/16"
-	ipv6Subnet   = "fd42:42:42::1/112"
-	addressBlock = "/32"
-	addressBlock6 = "/128"
+    wgInterface = "wg0"
+    ipv4Subnet   = "10.25.0.0/16"
+    ipv6Subnet   = "fd42:42:42::0/112"
 )
 
-type PeerConfig struct {
-	PrivateKey   string `json:"privatekey"`
-	Address      string `json:"address"`
-	PresharedKey string `json:"presharedkey"`
-	PublicKey    string `json:"publickey"`
-}
-
-func loadParams() (map[string]string, error) {
-	params := make(map[string]string)
-	file, err := os.Open(paramsPath)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var key, value string
-	for {
-		_, err := fmt.Fscanf(file, "%s=%s\n", &key, &value)
-		if err != nil {
-			break
-		}
-		params[key] = value
-	}
-	return params, nil
-}
-
-func execCommand(command string, args ...string) ([]byte, error) {
-	cmd := exec.Command(command, args...)
-	var out bytes.Buffer
-	cmd.Stdout = &out
-	err := cmd.Run()
-	if err != nil {
-		return nil, err
-	}
-	return out.Bytes(), nil
-}
-
-func getCurrentPeers() ([]string, error) {
-	out, err := execCommand(wgBinary, "show", "wg0", "allowed-ips")
-	if err != nil {
-		return nil, err
-	}
-
-	peers := strings.Split(string(out), "\n")
-	allocatedIPs := make([]string, 0, len(peers))
-
-	for _, peer := range peers {
-		if peer != "" {
-			parts := strings.Fields(peer)
-			if len(parts) > 1 {
-				allocatedIPs = append(allocatedIPs, parts[1])
-			}
-		}
-	}
-	return allocatedIPs, nil
-}
-
-func findNextIP(allocatedIPs []string, subnet string) (string, error) {
-	_, ipv4Net, _ := net.ParseCIDR(subnet)
-	ip := ipv4Net.IP
-	for {
-		ip = ipv4Net.IP.To4()
-		ip = nextIP(ip)
-		if ip == nil || !ipv4Net.Contains(ip) {
-			break
-		}
-
-		if !isIPAllocated(ip.String(), allocatedIPs) {
-			return ip.String(), nil
-		}
-	}
-	return "", fmt.Errorf("no available IPs in the subnet %s", subnet)
-}
-
-func nextIP(ip net.IP) net.IP {
-	ip = ip.To4()
-	for j := len(ip) - 1; j >= 0; j-- {
-		ip[j]++
-		if ip[j] != 0 {
-			break
-		}
-	}
-	return ip
-}
-
-func isIPAllocated(ip string, allocatedIPs []string) bool {
-	for _, allocated := range allocatedIPs {
-		if strings.HasPrefix(allocated, ip) {
-			return true
-		}
-	}
-	return false
-}
-
-func addPeer() error {
-	params, err := loadParams()
-	if err != nil {
-		return err
-	}
-
-	allocatedIPs, err := getCurrentPeers()
-	if err != nil {
-		return err
-	}
-
-	ipv4, err := findNextIP(allocatedIPs, ipv4Subnet)
-	if err != nil {
-		return err
-	}
-
-	ipv6, err := findNextIP(allocatedIPs, ipv6Subnet)
-	if err != nil {
-		return err
-	}
-
-	privateKeyOut, err := execCommand(wgBinary, "genkey")
-	if err != nil {
-		return err
-	}
-	privateKey := strings.TrimSpace(string(privateKeyOut))
-
-	publicKeyOut, err := execCommand(wgBinary, "pubkey")
-	if err != nil {
-		return err
-	}
-	publicKey := strings.TrimSpace(string(publicKeyOut))
-
-	presharedKeyOut, err := execCommand(wgBinary, "genpsk")
-	if err != nil {
-		return err
-	}
-	presharedKey := strings.TrimSpace(string(presharedKeyOut))
-
-	peerConf := PeerConfig{
-		PrivateKey:   privateKey,
-		Address:      fmt.Sprintf("%s%s, %s%s", ipv4, addressBlock, ipv6, addressBlock6),
-		PresharedKey: presharedKey,
-		PublicKey:    publicKey,
-	}
-
-	cmd := fmt.Sprintf(`wg set %s peer %s allowed-ips %s/32,%s/128`, params["SERVER_WG_NIC"], publicKey, ipv4, ipv6)
-	_, err = execCommand("bash", "-c", cmd)
-	if err != nil {
-		return err
-	}
-
-	output, err := json.Marshal(peerConf)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println(string(output))
-	return nil
-}
-
-func deletePeer(publicKey string) error {
-	params, err := loadParams()
-	if err != nil {
-		return err
-	}
-
-	cmd := fmt.Sprintf(`wg set %s peer %s remove`, params["SERVER_WG_NIC"], publicKey)
-	_, err = execCommand("bash", "-c", cmd)
-	if err != nil {
-		return err
-	}
-
-	fmt.Println("Peer deleted successfully.")
-	return nil
+type Peer struct {
+    PublicKey       string `json:"publickey"`
+    PrivateKey      string `json:"privatekey"`
+    Address         string `json:"address"`
+    PresharedKey    string `json:"presharedkey"`
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: wgraven <command> [args...]")
-		os.Exit(1)
-	}
+    if len(os.Args) < 2 {
+        fmt.Println("Usage: wgraven <command> [arguments]")
+        os.Exit(1)
+    }
 
-	command := os.Args[1]
+    switch os.Args[1] {
+    case "add":
+        addPeer()
+    case "delete":
+        if len(os.Args) != 3 {
+            fmt.Println("Usage: wgraven delete <publickey>")
+            os.Exit(1)
+        }
+        deletePeer(os.Args[2])
+    default:
+        fmt.Println("Unknown command:", os.Args[1])
+        os.Exit(1)
+    }
+}
 
-	switch command {
-	case "add":
-		if err := addPeer(); err != nil {
-			log.Fatal(err)
-		}
-	case "delete":
-		if len(os.Args) != 3 {
-			fmt.Println("Usage: wgraven delete <publickey>")
-			os.Exit(1)
-		}
-		publicKey := os.Args[2]
-		if err := deletePeer(publicKey); err != nil {
-			log.Fatal(err)
-		}
-	default:
-		fmt.Println("Unknown command")
-		os.Exit(1)
-	}
+func addPeer() {
+    ipv4Address, ipv6Address, err := findNextAvailableIPs()
+    if err != nil {
+        fmt.Println("Error finding available IPs:", err)
+        os.Exit(1)
+    }
+
+    privateKey, err := generateKey()
+    if err != nil {
+        fmt.Println("Error generating private key:", err)
+        os.Exit(1)
+    }
+
+    publicKey, err := generatePublicKey(privateKey)
+    if err != nil {
+        fmt.Println("Error generating public key:", err)
+        os.Exit(1)
+    }
+
+    presharedKey, err := generateKey()
+    if err != nil {
+        fmt.Println("Error generating preshared key:", err)
+        os.Exit(1)
+    }
+
+    err = addPeerToConfig(publicKey, privateKey, presharedKey, ipv4Address, ipv6Address)
+    if err != nil {
+        fmt.Println("Error adding peer to configuration:", err)
+        os.Exit(1)
+    }
+
+    peer := Peer{
+        PublicKey:    publicKey,
+        PrivateKey:   privateKey,
+        Address:      fmt.Sprintf("%s, %s", ipv4Address, ipv6Address),
+        PresharedKey: presharedKey,
+    }
+
+    output, err := json.MarshalIndent(peer, "", "  ")
+    if err != nil {
+        fmt.Println("Error marshalling JSON:", err)
+        os.Exit(1)
+    }
+
+    fmt.Println(string(output))
+}
+
+func deletePeer(publicKey string) {
+    err := removePeerFromConfig(publicKey)
+    if err != nil {
+        fmt.Println("Error deleting peer from configuration:", err)
+        os.Exit(1)
+    }
+
+    fmt.Println("Peer deleted successfully.")
+}
+
+func findNextAvailableIPs() (string, string, error) {
+    // List all existing peers
+    cmd := exec.Command("wg", "show", wgInterface, "allowed-ips")
+    output, err := cmd.Output()
+    if err != nil {
+        return "", "", err
+    }
+
+    existingIPs := parseIPAddresses(string(output))
+    nextIPv4, nextIPv6 := nextIP(existingIPs)
+
+    if nextIPv4 == "" || nextIPv6 == "" {
+        return "", "", fmt.Errorf("could not find available IPs")
+    }
+
+    return nextIPv4, nextIPv6, nil
+}
+
+func parseIPAddresses(output string) map[string]struct{} {
+    existingIPs := make(map[string]struct{})
+    lines := strings.Split(output, "\n")
+
+    for _, line := range lines {
+        if len(line) == 0 {
+            continue
+        }
+        parts := strings.Fields(line)
+        if len(parts) > 1 {
+            ipAddresses := strings.Split(parts[1], ",")
+            for _, ip := range ipAddresses {
+                existingIPs[ip] = struct{}{}
+            }
+        }
+    }
+
+    return existingIPs
+}
+
+func nextIP(existingIPs map[string]struct{}) (string, string) {
+    ipv4Addr, ipv6Addr := findNextIPv4(existingIPs), findNextIPv6(existingIPs)
+    return ipv4Addr, ipv6Addr
+}
+
+func findNextIPv4(existingIPs map[string]struct{}) string {
+    subnet := net.IPNet{
+        IP:   net.ParseIP("10.25.0.0"),
+        Mask: net.CIDRMask(16, 32),
+    }
+
+    for ip := subnet.IP.Mask(subnet.Mask); subnet.Contains(ip); incrementIP(ip) {
+        if _, exists := existingIPs[ip.String()]; !exists {
+            return ip.String() + "/32"
+        }
+    }
+
+    return ""
+}
+
+func findNextIPv6(existingIPs map[string]struct{}) string {
+    subnet := net.IPNet{
+        IP:   net.ParseIP("fd42:42:42::"),
+        Mask: net.CIDRMask(112, 128),
+    }
+
+    for ip := subnet.IP.Mask(subnet.Mask); subnet.Contains(ip); incrementIP(ip) {
+        if _, exists := existingIPs[ip.String()]; !exists {
+            return ip.String() + "/128"
+        }
+    }
+
+    return ""
+}
+
+func incrementIP(ip net.IP) {
+    for j := len(ip) - 1; j >= 0; j-- {
+        ip[j]++
+        if ip[j] != 0 {
+            break
+        }
+    }
+}
+
+func generateKey() (string, error) {
+    key := make([]byte, 32)
+    _, err := rand.Read(key)
+    if err != nil {
+        return "", err
+    }
+    return base64.StdEncoding.EncodeToString(key), nil
+}
+
+func generatePublicKey(privateKey string) (string, error) {
+    cmd := exec.Command("wg", "pubkey")
+    cmd.Stdin = strings.NewReader(privateKey)
+    output, err := cmd.Output()
+    if err != nil {
+        return "", err
+    }
+    return strings.TrimSpace(string(output)), nil
+}
+
+func addPeerToConfig(publicKey, privateKey, presharedKey, ipv4, ipv6 string) error {
+    cmd := exec.Command("wg", "set", wgInterface,
+        "peer", publicKey,
+        "preshared-key", presharedKey,
+        "endpoint", "YOUR_SERVER_IP:51820",
+        "allowed-ips", ipv4, ipv6,
+    )
+    return cmd.Run()
+}
+
+func removePeerFromConfig(publicKey string) error {
+    cmd := exec.Command("wg", "set", wgInterface, "peer", publicKey, "remove")
+    return cmd.Run()
 }
